f2c7bcf5726927fce0a8a87593f240a1e5b4d3a2
andreyshabalin <andrey.shabalin@gmail.com>
2016-09-05 16:39:39 -0400

    Added data prepare functions

--- R/fcp.R
+++ R/fcp.R
@@ -17,3 +17,21 @@ bitSumAndShifted = function(x, y, yoffset) {
 bitSumAndYinX = function(x, y, yoffset) {
 	.Call("CbitSumAndYinX", x, y, yoffset, PACKAGE = "fastCircularPermutations")
 }
+
+prepareBinaryDataLeft = function(set) {
+	n = length(set);
+	rez = packBits( c(set, set, rep(zero, (-n)%%32)), type = 'integer');
+	class(rez) = "fcpLeft";
+	return(rez);
+}
+
+prepareBinaryDataRight = function(set0) {
+	rez = vector('list', 32);
+	for( i in 0:31 ) {
+		# cat('i',i,'\n')
+		bin2[[i+1]] = packBits( c(rep(zero, i), set2, rep(zero, padding-i)), type = 'integer');
+	}
+	class(rez) = "fcpRight";
+	return(rez);
+}
+	
