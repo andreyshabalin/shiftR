7adab42a5a072de60fd1e9b9a2d151df75f8bc35
andreyshabalin <andrey.shabalin@gmail.com>
2016-09-05 18:50:40 -0400

    Fixed prepareBinaryData*.

--- R/fcp.R
+++ R/fcp.R
@@ -19,17 +19,30 @@ bitSumAndYinX = function(x, y, yoffset) {
 }
 
 prepareBinaryDataLeft = function(set) {
+	if(typeof(set) == "double")
+		set = as.integer(set);
+	zero = switch(typeof(set),
+					  "integer" = 0L,
+					  "logical" = FALSE,
+					  "raw" = as.raw(0));
 	n = length(set);
-	rez = packBits( c(set, set, rep(zero, (-n)%%32)), type = 'integer');
+	rez = packBits( c(set, set, rep(zero, (-2*n)%%32)), type = 'integer');
 	class(rez) = "fcpLeft";
 	return(rez);
 }
 
-prepareBinaryDataRight = function(set0) {
+prepareBinaryDataRight = function(set) {
+	if(typeof(set) == "double")
+		set = as.integer(set);
+	zero = switch(typeof(set),
+					  "integer" = 0L,
+					  "logical" = FALSE,
+					  "raw" = as.raw(0));
+	n = length(set);
 	rez = vector('list', 32);
 	for( i in 0:31 ) {
 		# cat('i',i,'\n')
-		bin2[[i+1]] = packBits( c(rep(zero, i), set2, rep(zero, padding-i)), type = 'integer');
+		rez[[i+1]] = packBits( c(rep(zero, i), set, rep(zero, (-i-n)%%32)), type = 'integer');
 	}
 	class(rez) = "fcpRight";
 	return(rez);
