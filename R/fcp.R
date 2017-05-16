092a391e296f0b60ca223b7d1e80980adb6132dd
andreyshabalin <andrey.shabalin@gmail.com>
2017-05-15 22:11:23 -0400

    Pull C code wrappers into a separate file.

--- R/fcp.R
+++ R/fcp.R
@@ -1,23 +1,3 @@
-bitSum = function(x){
-	.Call("CbitSum", x, PACKAGE = "fastCircularPermutations")
-}
-
-bitSumAnd = function(x,y){
-	.Call("CbitSumAnd", x, y, PACKAGE = "fastCircularPermutations")
-}
-
-bitSumOr = function(x,y){
-	.Call("CbitSumOr", x, y, PACKAGE = "fastCircularPermutations")
-}
-
-bitSumAndShifted = function(x, y, yoffset){
-	.Call("CbitSumAndShifted", x, y, yoffset, PACKAGE = "fastCircularPermutations")
-}
-
-bitSumAndYinX = function(x, y, yoffset){
-	.Call("CbitSumAndYinX", x, y, yoffset, PACKAGE = "fastCircularPermutations")
-}
-
 prepareBinaryDataLeft = function(set){
 	if(typeof(set) == "double")
 		set = as.integer(set);
