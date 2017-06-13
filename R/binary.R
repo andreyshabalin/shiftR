f88ae5a79adf39051af95c0a52a86146858c107f
andreyshabalin <andrey.shabalin@gmail.com>
2017-06-12 22:24:43 -0400

    rename r file for clarity

--- R/binary.R
+++ R/binary.R
@@ -1,19 +1,19 @@
-prepareBinaryDataLeft = function(set){
-	if(typeof(set) == "double")
-		set = as.integer(set);
-	zero = switch(typeof(set),
-					  "integer" = 0L,
-					  "logical" = FALSE,
-					  "raw" = as.raw(0));
-	n = length(set);
-	rez = packBits( c(set, set, rep(zero, (-2*n)%%32)), type = 'integer');
-	class(rez) = "fcpLeft";
-	attr(rez, "sum") = sum(set);
-	attr(rez, "len") = length(set);
-	return(rez);
+shiftrPrepareLeft = function(set){
+    if(typeof(set) == "double")
+    	set = as.integer(set);
+    zero = switch(typeof(set),
+        "integer" = 0L,
+        "logical" = FALSE,
+        "raw" = as.raw(0));
+    n = length(set);
+    rez = packBits( c(set, set, rep(zero, (-2*n)%%32)), type = 'integer');
+    class(rez) = "fcpLeft";
+    attr(rez, "sum") = sum(set);
+    attr(rez, "len") = length(set);
+    return(rez);
 }
 
-prepareBinaryDataRight = function(set){
+shiftrPrepareRight = function(set){
 	if(typeof(set) == "double")
 		set = as.integer(set);
 	zero = switch(typeof(set),
@@ -39,6 +39,10 @@ singleCircularPermutation = function(left, right, offset){
 	stopifnot( offset >= 0L )
 	stopifnot( offset < attr(right, "len") )
 	.Call("CbitSumAndYinX", left, right[[1 + (offset) %% 32]], offset %/% 32, PACKAGE = "shiftR")
+}
+
+getNOffestsMax = function(n, margin){
+    as.integer(n*(1-margin)) - as.integer(n*margin) + 1L;
 }
 
 # n = 100; margin = 0.05; npermute = 13
@@ -81,7 +85,7 @@ getOffsetsUniform = function(n, npermute, margin = 0.05){
 
 shiftrPermBinary = function(left, right, offsets, alsoDoFisher = TRUE, returnPermOverlaps = FALSE){
 	rez = list();
-	stopifnot( typeof(offsets) == "integer" );
+	stopifnot( typeof(offsets) %in% c("NULL","integer") );
 	stopifnot( class(left) == "fcpLeft" );
 	stopifnot( class(right) == "fcpRight" );
 	stopifnot( attr(left, "len") == attr(right, "len") );
@@ -96,32 +100,39 @@ shiftrPermBinary = function(left, right, offsets, alsoDoFisher = TRUE, returnPer
 					  fisherMat = fisherMat)
 	}
 
-	overlapsPerm = integer(length(offsets));
-	for( i in seq_along(offsets) ) 
-		overlapsPerm[i] = .Call("CbitSumAndYinX", left, right[[1 + (offsets[i]) %% 32]], offsets[i] %/% 32, PACKAGE = "shiftR")
-	permPVenrich  = max(mean(overlapsPerm >= sum12), 0.5/length(offsets));
-	permPVdeplete = max(mean(overlapsPerm <= sum12), 0.5/length(offsets));
-	permPV = min(permPVenrich, permPVdeplete, 0.5)*2;
-	
-	meanO = mean(overlapsPerm);
-	stdO = sd(overlapsPerm);
-	
-	permZ = (sum12 - meanO) / stdO;
+	if(length(offsets)>0L){
+    	overlapsPerm = integer(length(offsets));
+    	for( i in seq_along(offsets) ) 
+    		overlapsPerm[i] = .Call("CbitSumAndYinX", left, right[[1 + (offsets[i]) %% 32]], offsets[i] %/% 32, PACKAGE = "shiftR")
+    	permPVenrich  = max(mean(overlapsPerm >= sum12), 0.5/length(offsets));
+    	permPVdeplete = max(mean(overlapsPerm <= sum12), 0.5/length(offsets));
+    	permPV = min(permPVenrich, permPVdeplete, 0.5)*2;
+    	
+    	meanO = mean(overlapsPerm);
+    	stdO = sd(overlapsPerm);
+    	
+    	permZ = (sum12 - meanO) / stdO;
+    	
+    	rezPerm = list(
+    	    permPVenrich = permPVenrich,
+    	    permPVdeplete = permPVdeplete,
+    	    permPV = permPV,
+    	    permZ = permZ);
+	} else {
+	    rezPerm = NULL;
+	}
 
-	rez = c(rez,list(
-		nfeatures = len,
-		lfeatures = sum1,
-		rfeatures = sum2,
-		overlap = sum12,
-		overlapUnderNull = sum1 / len * sum2,
-		oddsRatio = sum12 * (len - sum1 - sum2 + sum12 + 0) /
-		            ((sum1-sum12) * (sum2-sum12)),
-		permPVenrich = permPVenrich,
-		permPVdeplete = permPVdeplete,
-		permPV = permPV,
-		permZ = permZ
-		));
-	if(returnPermOverlaps) {
+	rez = c(rez, 
+	        list(
+        		nfeatures = len,
+        		lfeatures = sum1,
+        		rfeatures = sum2,
+        		overlap = sum12,
+        		overlapUnderNull = sum1 / len * sum2,
+        		oddsRatio = sum12 * as.numeric(len - sum1 - sum2 + sum12) /
+        		            (as.numeric(sum1-sum12) * (sum2-sum12))),
+    		rezPerm);
+	if(returnPermOverlaps){
 		rez = c(list(overlapsPerm = overlapsPerm), rez);
 	}
 	return(rez);
