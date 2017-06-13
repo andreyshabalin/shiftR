ff446d7e893d2fd9c051cceab8e0b688a08c4168
andreyshabalin <andrey.shabalin@gmail.com>
2017-06-13 01:03:35 -0400

    Fix rounding treatment in getOffsets.

--- R/getOffsets.r
+++ R/getOffsets.r
@@ -1,34 +1,42 @@
+minOffset = function(n, margin){
+    as.integer(ceiling(n*margin));
+}
+
+maxOffset = function(n, margin){
+    as.integer(floor(n*(1-margin)));
+}
+
 getNOffsetsMax = function(n, margin){
-    as.integer(n*(1-margin)) - as.integer(n*margin) + 1L;
+    maxOffset(n, margin) - minOffset(n, margin) + 1L;
 }
 
 getOffsetsAll = function(n, margin){
+    stopifnot( is.numeric(margin) );
     stopifnot( margin >= 0 );
     stopifnot( margin < 0.5 );
-    stopifnot( is.numeric(margin) );
     stopifnot( is.numeric(n) );
-    return( as.integer(n*margin):as.integer(n*(1-margin)) );
+    return( ceiling(n*margin):as.integer(n*(1-margin)) );
 }
 
 getOffsetsRandom = function(n, npermute, margin = 0.05){
+    stopifnot( is.numeric(margin) );
     stopifnot( margin >= 0 );
     stopifnot( margin < 0.5 );
-    stopifnot( is.numeric(margin) );
     stopifnot( is.numeric(n) );
     stopifnot( is.numeric(npermute) );
-    offsets = sample.int(n = floor(n*(1-2*margin))+1, size = floor(npermute), replace = FALSE) + as.integer(n*margin-1);
+    offsets = sample.int(n = getNOffsetsMax(n, margin), size = floor(npermute), replace = FALSE) + (minOffset(n, margin)-1L);
     return( offsets );
 }
 
 getOffsetsUniform = function(n, npermute, margin = 0.05){
+    stopifnot( is.numeric(margin) );
     stopifnot( margin >= 0 );
     stopifnot( margin < 0.5 );
-    stopifnot( is.numeric(margin) );
     stopifnot( is.numeric(n) );
     stopifnot( is.numeric(npermute) );
-    from = as.integer(n*margin)
-    to = as.integer(n*(1-margin))
-    stopifnot( npermute <= to - from );
-    offsets = as.integer(seq.int(from = from, to = to, length.out = npermute));
+    fr = minOffset(n, margin)
+    to = maxOffset(n, margin)
+    stopifnot( npermute <= to - fr + 1);
+    offsets = as.integer(seq.int(from = fr, to = to, length.out = npermute));
     return( offsets );
 }
