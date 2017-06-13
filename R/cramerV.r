e9dc172e17f4eb3b09ce0c002f38bb387dd504a8
andreyshabalin <andrey.shabalin@gmail.com>
2017-06-12 22:18:02 -0400

    Add function for Cramer's (V) coefficient.

---
+++ R/cramerV.r
@@ -0,0 +1,8 @@
+cramerV = function(sum12, sum1, sum2, len){
+    # Cramer's (V) coefficient
+    # https://en.wikipedia.org/wiki/Phi_coefficient
+    # http://www.aliquote.org/pub/3531_ftp.pdf
+    len = as.numeric(len);
+    sqrt(len / (sum1 * (len - sum1) * (sum2) * (len - sum2))) * 
+        (sum12 * (len + (sum12- sum1 - sum2)) - as.numeric(sum1-sum12) * (sum2-sum12));
+}
