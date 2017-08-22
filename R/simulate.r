253b2cf663e10f0565c0e166f0d6055ef187eee5
andreyshabalin <andrey.shabalin@gmail.com>
2017-08-22 02:11:43 -0400

    Added simulation functions, with manual.

---
+++ R/simulate.r
@@ -0,0 +1,25 @@
+simulateAR = function(n, cor){
+    rn = rnorm(n);
+    mult = sqrt(1 - cor^2);
+    for( i in 2:n ){
+        rn[i] = rn[i-1] * cor + rn[i] * mult;
+    }
+    return(rn);
+}
+
+simulateNumeric = function(n, corWithin, corAcross = 0){
+    stopifnot(n>1)
+    
+    data1 = simulateAR(n, corWithin);
+    data2 = simulateAR(n, corWithin);
+  
+    if(corAcross != 0){
+        data2 = data1 * corAcross + data2 * sqrt(1 - corAcross^2);
+    }
+    return( list( data1 = data1, data2 = data2) );
+}
+
+simulateBinary = function(n, corWithin, corAcross = 0){
+    tmp = simulateNumeric(n, corWithin, corAcross = 0)
+    return( list( data1 = as.integer(tmp$data1>0), data2 = as.integer(tmp$data2>0)) );
+}
